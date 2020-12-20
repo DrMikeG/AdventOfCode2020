9eab0bb404b0aae54e279935ccfe8825ba67e8e7
Mike Gibbens <mike.gibbens@siemens.com>
2020-12-20 12:11:49 +0000

    20th Dec problems rotating edges

--- 20a/tool_src/advent.py
+++ 20a/tool_src/advent.py
@@ -18,6 +18,7 @@ class Tile:
 
     def __init__(self, lines):
         self.default_edges = []
+        self.flipped_edges = []
         self.rotations = []
         self.id = -1
         assert len(lines) == 11
@@ -59,21 +60,93 @@ class Tile:
         assert len(self.default_edges) == 4
         return self.default_edges
 
+    def getFlippedCharsArrays(self):
+        assert len(self.flipped_edges) == 4
+        return self.flipped_edges
+
     def processsLinesIntoDefaultEdges(self,lines):
         # Get the 4 border strings into self.default_edges
         self.calcCharsArrayFromLines(lines)
+
+    def flipDefaultHorizontally(self):
+        chars = self.getCharsArrays()
+        assert len(chars) == 4 # N E S W
+        self.flipped_edges = []
+        self.flipped_edges.append(chars[0][::-1])   # North is read right to left
+        self.flipped_edges.append(chars[3])         # West replaces East
+        self.flipped_edges.append(chars[2][::-1])   # South is read right to left
+        self.flipped_edges.append(chars[1])         # East replaces West
 
     def defaultFourInts(self):
         fourInts = []
         for st in self.default_edges:
             fourInts.append(self.charStringToInt(st))
         return fourInts
+    
+    def flippedFourInts(self):
+        fourInts = []
+        for st in self.flipped_edges:
+            fourInts.append(self.charStringToInt(st))
+        return fourInts
 
     def calculateRotations(self):
         # process default edges to make 8 permutations
         # First entry
+        # Default edge strings (read left-right, top-bottom)
+        N_S_E_W_Edges = self.default_edges
+        # Reversed edge strings (read right-left, bottom-top)
+        rN_S_E_W_Edges = [N_S_E_W_Edges[0][::-1],N_S_E_W_Edges[1][::-1],N_S_E_W_Edges[2][::-1],N_S_E_W_Edges[3][::-1]]
+
         self.rotations.append(self.defaultFourInts())
+        # The default four ints are read left to right for N and S
+        # Top to bottom for E and W.
+        # If we rotate the image 90 degrees anti-clockwise,
+        # Then E is now at the top, and is correctly read left to right (== top to bottom)
+        # S is now E, but needs to be read top to bottom (which is the reverse of how it was previously read)
+        # W is now S, and is correctly read left to right (== top to bottom)
+        # N is now W, but needs to be read top to bottom (which is the reverse of how it was previously read)
+
+        ## Make ESWN (rotation once anti-clockwise)
+        self.rotations.append( [ 
+            self.charStringToInt( N_S_E_W_Edges[1]), # new north = East in standard reading
+            self.charStringToInt(rN_S_E_W_Edges[2]), # new east  = South in reverse reading
+            self.charStringToInt( N_S_E_W_Edges[3]), # new south = West in standard reading
+            self.charStringToInt(rN_S_E_W_Edges[0]), # new west = North in reverse reading
+        ] )
+        
+        ## Make SWNE (rotation once anti-clockwise)
+        self.rotations.append( [ 
+            self.charStringToInt(rN_S_E_W_Edges[2]), # new north = South in reverse reading
+            self.charStringToInt(rN_S_E_W_Edges[3]), # new east  = West in reverse reading
+            self.charStringToInt(rN_S_E_W_Edges[0]), # new south = North in reverse reading
+            self.charStringToInt(rN_S_E_W_Edges[1]), # new west =  East in reverse reading
+        ] )
+        
+        ## Make WNES (rotation once anti-clockwise)
+        self.rotations.append( [ 
+            self.charStringToInt(rN_S_E_W_Edges[3]), # new north = WEST in reverse reading
+            self.charStringToInt( N_S_E_W_Edges[0]), # new east  = NORTH in standard reading
+            self.charStringToInt(rN_S_E_W_Edges[1]), # new south = EAST in reverse reading
+            self.charStringToInt( N_S_E_W_Edges[2]), # new west = SOUTH in standard reading
+        ] )
+
     
+    def calculateCycleRotations(self,n):
+        assert n == 0 or n == 4
+        listToCycle = self.rotations[0].copy()
+        assert len(listToCycle) == 4
+
+        for _ in [0,1,2]:
+            save = listToCycle.pop(0)
+            listToCycle.append(save)
+            self.rotations.append(listToCycle.copy())
+
+        save = listToCycle.pop(0)
+        listToCycle.append(save)
+        assert listToCycle == self.rotations[0] # End up back where you started
+        assert len(self.rotations) == 4 or len(self.rotations) == 8
+
+
     def getRotations(self):
         return self.rotations
 
--- 20a/tool_src/test_unit.py
+++ 20a/tool_src/test_unit.py
@@ -21,15 +21,29 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(charArrays[2],"..###..###")
         self.assertEqual(charArrays[3],".#####..#.")
         rotations = tiles[0].getRotations()
-        self.assertEqual(1,len(rotations))
+        self.assertEqual(4,len(rotations))
+
         defaultRotation = rotations[0]
-        self.assertEqual(4,len(defaultRotation))        
-        self.assertEqual(int("0011010010",2),defaultRotation[0]) # 210
-        self.assertEqual(int("0001011001",2),defaultRotation[1]) # 89
-        self.assertEqual(int("0011100111",2),defaultRotation[2]) #231
-        self.assertEqual(int("0111110010",2),defaultRotation[3]) # 498
+        self.assertEqual(4,len(defaultRotation))
+        self.assertEqual(int("0011010010",2),defaultRotation[0]) # 210 N
+        self.assertEqual(int("0001011001",2),defaultRotation[1]) # 89  E
+        self.assertEqual(int("0011100111",2),defaultRotation[2]) #231  S
+        self.assertEqual(int("0111110010",2),defaultRotation[3]) # 498 W
 
+        ESWN = rotations[1]
+        self.assertEqual(4,len(ESWN))
+        self.assertEqual(int("0001011001",2),ESWN[0]) # 89  E
+        self.assertEqual(int("1110011100",2),ESWN[1]) # 924  rS
+        self.assertEqual(int("0111110010",2),ESWN[2]) # 498 W
+        self.assertEqual(int("0100101100",2),ESWN[3]) # 300 rN
 
+        SWNE = rotations[2]
+        self.assertEqual(4,len(SWNE))
+        self.assertEqual(int("1110011100",2),SWNE[0]) # 924 rS
+        self.assertEqual(int("1011000001",2),SWNE[1]) # 705 rW
+        self.assertEqual(int("0100101100",2),SWNE[2]) # 300 N
+        self.assertEqual(int("0001011001",2),SWNE[3]) # 89  E
+        
 
 
 
